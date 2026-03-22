9788483c660c206140d6b986d6d0872038510829
fatimabenzaoui <fatima.benzaoui@yahoo.fr>
2026-03-22 20:00:15 +0100

    Ajout fallback pour l'URL de l'API en production

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  baseURL: import.meta.env.VITE_PUBLIC_API_URL || "https://fatimabenzaoui.pythonanywhere.com/",
   headers: {
     "Content-Type": "application/json",
   },
